1914fe48457a143ce60b20e57504fb10abcdeeb5
hoangviethung <hoangviet.hung.009@gmail.com>
2019-08-17 14:33:34 +0700

    HÙNG ADD SOURCE

---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+// Function thêm class lazyload vào các thẻ <img> có thuộc tính [data-src]
+const addClassLazyload = () => {
+	let imgList = document.querySelectorAll("img[data-src]")
+	Array.prototype.forEach.call(imgList, function (el) {
+		if (el.className.length > 0) {
+			el.className = el.className + " lazyload"
+		} else {
+			el.className = "lazyload"
+		}
+	});
+}
+var swiper = new Swiper('.slider-Homebanner', {
+	effect: 'coverflow',
+	slidesPerView: 1,
+	grabCursor: true,
+	speed: 1500,
+	simulateTouch: false,
+	autoplay: {
+		delay: 8000,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
+$(document).ready(function () {
+	objectFitImages("img.ofc"); // Luôn luôn chậy polyfill cho thuôc tính object-fit: cover trên các phiên bản IE >= 9
+	addClassLazyload(); // Luôn luôn addClass lazyload cho các hình ảnh có thuộc tính [data-src]
+})
+
+$(document).ajaxComplete(function () {
+	addClassLazyload();
+})
